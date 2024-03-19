b5c7e65618a00426f817ff7529efbe917df059c7
Andrea F. Daniele <afdaniele@duckietown.com>
2024-03-18 23:54:07 -0400

    added DataDecodingError exception

--- src/duckietown_messages/base.py
+++ src/duckietown_messages/base.py
@@ -1,9 +1,10 @@
 import typing
 from abc import ABCMeta
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError
 
 from dtps_http import RawData
+from duckietown_messages.utils.exceptions import DataDecodingError
 
 
 class BaseMessage(BaseModel, metaclass=ABCMeta):
@@ -11,8 +12,11 @@ class BaseMessage(BaseModel, metaclass=ABCMeta):
     @classmethod
     def from_rawdata(cls, rd: RawData) -> 'BaseMessage':
         data: dict = typing.cast(dict, rd.get_as_native_object())
-        # noinspection PyArgumentList
-        return cls(**data)
+        try:
+            # noinspection PyArgumentList
+            return cls(**data)
+        except ValidationError as e:
+            raise DataDecodingError(f"Error while parsing {cls.__name__} from {rd}: {e}", e)
 
     def to_rawdata(self) -> RawData:
         return RawData.cbor_from_native_object(self.dict())
