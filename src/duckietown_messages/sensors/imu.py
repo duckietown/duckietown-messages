4fa9e46195d9f85022350da3012367e324ff577a
tuxliri <dvde.iafrate98@gmail.com>
2024-04-17 12:46:52 +0200

    Added imu message

--- src/duckietown_messages/sensors/imu.py
+++ src/duckietown_messages/sensors/imu.py
@@ -1,19 +1,22 @@
-from duckietown_messages.standard.header import AUTO
-from duckietown_msgs.msg import BaseMessage
-from geometry_msgs.msg import Quaternion, Vector3
+from typing import List, Optional
+from pydantic import Field
+
+from ..base import BaseMessage
+from ..standard.header import Header, AUTO
+from duckietown_messages.geometry_3d.quaternion import Quaternion
+from duckietown_messages.sensors.angular_velocities import AngularVelocities
+from duckietown_messages.sensors.linear_accelerations import LinearAccelerations
 
 class Imu(BaseMessage):
     """Imu message, used to store the IMU data.
     Refer to the ROS message definition for more information.
     http://docs.ros.org/en/noetic/api/sensor_msgs/html/msg/Imu.html
     """
+    header : Header = AUTO
     
-    def __init__(self):
-        super(Imu, self).__init__()
-        self.header = AUTO
-        self.orientation = Quaternion()
-        self.orientation_covariance = [0.0] * 9
-        self.angular_velocity = Vector3()
-        self.angular_velocity_covariance = [0.0] * 9
-        self.linear_acceleration = Vector3()
-        self.linear_acceleration_covariance = [0.0] * 9
+    orientation : Optional[Quaternion]
+    orientation_covariance : Optional[List[float]] = Field(max_items=9, min_items=9)
+    angular_velocity : Optional[AngularVelocities]
+    angular_velocity_covariance : Optional[List[float]] = Field(max_items=9, min_items=9)
+    linear_acceleration : Optional[LinearAccelerations]
+    linear_acceleration_covariance : Optional[List[float]] = Field(max_items=9, min_items=9)
