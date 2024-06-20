6e7396354a340871cc7e73c50b961c49ef299782
Andrea F. Daniele <afdaniele@duckietown.com>
2024-06-20 17:23:15 -0400

    added BaseMessage.from_rawdata(..., allow_none: bool)

--- src/duckietown_messages/base.py
+++ src/duckietown_messages/base.py
@@ -12,8 +12,15 @@ class BaseMessage(BaseModel, metaclass=ABCMeta):
     # TODO: add a field for the header and remove it from the subclasses
 
     @classmethod
-    def from_rawdata(cls, rd: RawData) -> 'BaseMessage':
-        data: dict = typing.cast(dict, rd.get_as_native_object())
+    def from_rawdata(cls, rd: RawData, allow_none: bool = False) -> 'BaseMessage':
+        native: object = rd.get_as_native_object()
+        if native is None:
+            if allow_none:
+                # noinspection PyTypeChecker
+                return None
+            raise DataDecodingError(f"Expected a dict-like object, received None instead")
+        # ---
+        data: dict = typing.cast(dict, native)
         try:
             # noinspection PyArgumentList
             return cls(**data)
